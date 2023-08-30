88d7286e0d87f2ef7815478264b1edc033079ad2
cchhdcc <cchhdcc@163.com>
2023-08-30 21:35:33 +0800

    feat:添加main.m

---
+++ main.m
@@ -0,0 +1,9 @@
+clear
+clc
+%% 运行测试样例
+%I = imread('测试图像\Airplane.tiff');
+%I = imread('测试图像\Tiffany.tiff');
+I = imread('测试图像\Lena.tiff');
+%I = imread('测试图像\Man.tiff');
+%I = imread('测试图像\Baboon.tiff');
+%I = imread('测试图像\Jetplane.tiff');
